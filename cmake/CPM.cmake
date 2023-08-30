e932f05c50dac51be7e547217e5bd8cf1b154388
Claus <claus.klein@rohde-schwarz.com>
2023-08-30 11:55:29 +0200

    OSX does not support ENABLE_SANITIZER_LEAK
    Merge branch 'develop-patch-1613' into 'feature/setup-ci'
    
    Update file CPM.cmake
    
    Update github ci workflow
    
    Use cache for ccache too
    
    Update pip too
    
    Use cpack for Release build on CI

--- cmake/CPM.cmake
+++ cmake/CPM.cmake
@@ -18,6 +18,18 @@ get_filename_component(CPM_DOWNLOAD_LOCATION ${CPM_DOWNLOAD_LOCATION} ABSOLUTE)
 
 function(download_cpm)
     message(STATUS "Downloading CPM.cmake to ${CPM_DOWNLOAD_LOCATION}")
+    set(GITHUB_MIRROR "https://code.rsint.net/mirror/github.com")
+    file(DOWNLOAD ${GITHUB_MIRROR}/cpm-cmake/CPM.cmake/-/raw/v${CPM_DOWNLOAD_VERSION}/cmake/CPM.cmake
+         ${CPM_DOWNLOAD_LOCATION}
+    )
+    # Version number needs to be patched.
+    file(READ ${CPM_DOWNLOAD_LOCATION} FILE_CONTENT)
+    string(REPLACE "1.0.0-development-version" "${CPM_DOWNLOAD_VERSION}" FILE_CONTENT "${FILE_CONTENT}")
+    file(WRITE ${CPM_DOWNLOAD_LOCATION} "${FILE_CONTENT}")
+endfunction()
+
+function(download_cpm_orig)
+    message(STATUS "Downloading CPM.cmake to ${CPM_DOWNLOAD_LOCATION}")
     file(
         DOWNLOAD
         https://github.com/cpm-cmake/CPM.cmake/releases/download/v${CPM_DOWNLOAD_VERSION}/CPM.cmake
