27d8c194af71565f133542f4c6fcb291e3734089
ClausKlein <claus.klein@arcormail.de>
2023-08-31 06:07:32 +0200

    update git config settings for gitlab CI
    Setup githup mirror on gitlab
    
    Fix cpm bootstrap on CI
    
    Use Workaround to prevent load of submodules
     Needed as ProjectOptions try to load an example as git submodue.
    
    Prevent load of unused packages

--- cmake/CPM.cmake
+++ cmake/CPM.cmake
@@ -18,18 +18,6 @@ get_filename_component(CPM_DOWNLOAD_LOCATION ${CPM_DOWNLOAD_LOCATION} ABSOLUTE)
 
 function(download_cpm)
     message(STATUS "Downloading CPM.cmake to ${CPM_DOWNLOAD_LOCATION}")
-    set(GITHUB_MIRROR "https://code.rsint.net/mirror/github.com")
-    file(DOWNLOAD ${GITHUB_MIRROR}/cpm-cmake/CPM.cmake/-/raw/v${CPM_DOWNLOAD_VERSION}/cmake/CPM.cmake
-         ${CPM_DOWNLOAD_LOCATION}
-    )
-    # Version number needs to be patched.
-    file(READ ${CPM_DOWNLOAD_LOCATION} FILE_CONTENT)
-    string(REPLACE "1.0.0-development-version" "${CPM_DOWNLOAD_VERSION}" FILE_CONTENT "${FILE_CONTENT}")
-    file(WRITE ${CPM_DOWNLOAD_LOCATION} "${FILE_CONTENT}")
-endfunction()
-
-function(download_cpm_orig)
-    message(STATUS "Downloading CPM.cmake to ${CPM_DOWNLOAD_LOCATION}")
     file(
         DOWNLOAD
         https://github.com/cpm-cmake/CPM.cmake/releases/download/v${CPM_DOWNLOAD_VERSION}/CPM.cmake
@@ -37,8 +25,28 @@ function(download_cpm_orig)
     )
 endfunction()
 
+function(download_cpm_CI)
+    message(STATUS "Downloading CPM.cmake ON CI to ${CPM_DOWNLOAD_LOCATION}")
+    set(GITHUB_MIRROR "https://code.rsint.net/mirror/github.com")
+    file(
+        DOWNLOAD
+        ${GITHUB_MIRROR}/cpm-cmake/CPM.cmake/-/raw/v${CPM_DOWNLOAD_VERSION}/cmake/CPM.cmake
+        ${CPM_DOWNLOAD_LOCATION}
+    )
+    # Version number needs to be patched.
+    file(READ ${CPM_DOWNLOAD_LOCATION} FILE_CONTENT)
+    string(REPLACE "1.0.0-development-version" "${CPM_DOWNLOAD_VERSION}"
+                   FILE_CONTENT "${FILE_CONTENT}"
+    )
+    file(WRITE ${CPM_DOWNLOAD_LOCATION} "${FILE_CONTENT}")
+endfunction()
+
 if(NOT (EXISTS ${CPM_DOWNLOAD_LOCATION}))
-    download_cpm()
+    if(DEFINED ENV{CICD_USER})
+        download_cpm_ci()
+    else()
+        download_cpm()
+    endif()
 else()
     # resume download if it previously failed
     file(READ ${CPM_DOWNLOAD_LOCATION} check)
